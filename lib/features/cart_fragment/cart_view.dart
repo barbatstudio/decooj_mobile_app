01968c952eaf682a298cb9e93d6dfa1cf459afdf
farid fajrak <faridfajrak@gmail.com>
2021-03-14 13:26:59 +0330

    bascket fragment minor UI bug fixed

--- lib/features/cart_fragment/cart_view.dart
+++ lib/features/cart_fragment/cart_view.dart
@@ -11,49 +11,51 @@ class CartView extends StatelessWidget {
     return ViewModelProvider<CartViewModel>(
         viewModel: CartViewModel(context: context),
         builder: (viewModel) {
-          return Center(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.7,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [kBoxShadow]
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10)),
-                      color: kGreenColor,
-                    ),
-                    child: Text(
-                      "سبد خرید",
-                      style: kTitleTextStyle.copyWith(color: Colors.white),
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
+          return SingleChildScrollView(
+            child: Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.7,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [kBoxShadow]
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
-                        color: Colors.white,
-                        image: DecorationImage(
-                            image: AssetImage('images/main_slider_bg.png'),
-                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10)),
+                        color: kGreenColor,
                       ),
-                      child: Center(child: viewModel.cartListView),
+                      child: Text(
+                        "سبد خرید",
+                        style: kTitleTextStyle.copyWith(color: Colors.white),
+                        textAlign: TextAlign.end,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
+                          color: Colors.white,
+                          image: DecorationImage(
+                              image: AssetImage('images/main_slider_bg.png'),
+                              fit: BoxFit.cover),
+                        ),
+                        child: Center(child: viewModel.cartListView),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
 
-                ],
+                  ],
+                ),
               ),
             ),
           );
